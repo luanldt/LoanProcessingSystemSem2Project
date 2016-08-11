8e25dd810a9969f3f07fce477606dab12c777fe0
cuongtt2109 <cuongtt2109@gmail.com>
2016-08-11 10:28:35 +0700

    cuongtt remove Voucher

--- src/dao/VoucherDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import entities.VoucherDetail;
-
-public class VoucherDetailDAO extends AbstractModel<VoucherDetail>{
-	public VoucherDetailDAO() {
-		super(VoucherDetail.class);
-	}
-}
